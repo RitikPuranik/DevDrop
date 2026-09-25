f496e04fa269a85ed5a7c36557a4d5430196f733
Ritik Puranik <ritikpuranik28@gmail.com>
2026-09-26 00:44:16 +0530

    perf: remove PostHog from initial bundle

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { PostHogProvider } from '@posthog/react'
+import PostHogProvider from './analytics/PostHogProvider'
 import * as Sentry from "@sentry/react";
 
 if (import.meta.env.VITE_SENTRY_DSN) {
